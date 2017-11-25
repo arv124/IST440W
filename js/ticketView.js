476b33d8437f30998349a2229ce0bac58f7637e9
kyleisryan <kjryan570@gmail.com>
2017-11-25 10:58:55 -0500

    added basic logic and stubs to ticketView

--- js/ticketView.js
+++ js/ticketView.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
-	$('#searchFilter').on('click', function (event) {
+
+	var database = firebase.database();
+	var ticketRef = database.ref('TicketSystem/Ticket');
+	var ticketDiv;
+	var tickets;
+
+	ticketRef.once("value", function(data){
+
+		tickets = data.val();
+
+		$.each(tickets, function(i, item){
+			$('#listBody').html(function(index, currentcontent){
+				//For each element in the array create a div in listBody
+			});
+		});
+
+	});
+
+	$('#searchFilter').click(function(event){
 		event.stopPropagation();
-		console.log("propagation stopped");
+	});
+
+	$('#logout').click(function(){
+		firebase.auth().signOut();
 	});
 });
