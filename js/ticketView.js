e059ebb2c88899d83b3cf0371b423525c62f9d7c
kyleisryan <kjryan570@gmail.com>
2017-11-29 13:47:58 -0500

    Changing up how the ticketView is going to work, in the midst of  adding logic to the table so rows will be dynamic

--- js/ticketView.js
+++ js/ticketView.js
@@ -1,18 +1,48 @@
 $(document).ready(function(){
-
 	var database = firebase.database();
 	var ticketRef = database.ref('TicketSystem/Ticket');
-	var ticketDiv;
+	//var empRef = database.ref('Employees');
+	var employeeID;
+	var ticketHTML;
+	var ticketID;
 	var tickets;
+	/*
+	empRef.once("value", function(data){
 
+		var employee = data.val();
+
+		$.each(employee, function(i, item){
+			if(employee[i].email === user.email){
+				employeeID = employee[i].email;
+				//console.log("ID: "+employeeID)
+			}
+		});
+	});
+	*/
 	ticketRef.once("value", function(data){
 
 		tickets = data.val();
-
 		$.each(tickets, function(i, item){
-			$('#listBody').html(function(index, currentcontent){
-				//For each element in the array create a div in listBody
-			});
+				if(tickets[i].status === "open"){
+					ticketID = tickets[i].ticketID;
+					//For each element in the array create a div in listBody
+					/*
+					ticketHTML = "<tr id='ticket" + ticketID + ">" +
+									"<td>" + ticketID + "</td>" +
+									"<td>" + tickets[i].customer + "</td>" +
+									"<td>" + tickets[i].description + "</td>" +
+									"<td>" + tickets[i].contactInfo + "</td>" +
+									"<td>" + tickets[i].status + "</td>" +
+									"<td>" + tickets[i].openDate + "</td>" +
+									"<td>" + tickets[i].priority + "</td>" +
+								 "</tr>";
+					*/
+					$('#ticketTable').find('tableBody')
+						.append(ticketHTML)
+							.append('<tr>')
+								.append('<td>').text(ticketID)
+					console.log("ticket appended")
+				}
 		});
 
 	});
